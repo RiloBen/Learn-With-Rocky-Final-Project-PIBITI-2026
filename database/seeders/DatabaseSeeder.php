78506f8840339d6af1d53149c1f6f3cde772a218
RiloBen <rilobenaya@gmail.com>
2026-06-12 18:58:27 +0700

    feat: update DatabaseSeeder to create a test user with default username

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'username' => 'admin',
-            'theme_preference' => 'adrian',
+            'username' => 'test_user',
         ]);
     }
 }
